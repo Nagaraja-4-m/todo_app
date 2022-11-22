d97b6338edd717668a168068c9b464ed56d1d6b9
Nagaraja M <89149882+Nagaraja-4-m@users.noreply.github.com>
2022-11-22 21:31:09 +0530

    Update settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qr81!uhny2t8zuj3_ead)g8p-*hib_#$vnskd7hss&v9jnzhw0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['todo-django01.herokuapp.com']
 
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static_files/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 # STATICFILES_DIRS = ((os.path.join(BASE_DIR, 'static')), )
 
